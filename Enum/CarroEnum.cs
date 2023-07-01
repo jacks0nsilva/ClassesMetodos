dffa5d6fb66ba2b601cb76e2083d6be9f5af94b9
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-07-01 12:56:26 -0300

    enum

---
+++ Enum/CarroEnum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Carro
+    {
+        public string? Modelo { get; set; }
+        public string? Montadora { get; set; }
+        public string? Marca { get; set; }
+        public int Ano { get; set; }
+        public int Potencia { get; set; }
+        public int Cor;
+        public Carro(string modelo, string montadora, string marca, int ano, int potencia, int cor)
+        {
+            Modelo = modelo;
+            Montadora = montadora;
+            Marca = marca;
+            Ano = ano;
+            Potencia = potencia;
+            Cor = cor;
+        }
+
+        public void ExibirInfo()
+        {
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Montadora: {Montadora}");
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Potência: {Potencia}");
+            Console.WriteLine($"Ano: {Ano}");
+            Console.WriteLine($"Cor: {(Cores)Cor}");
+        }
+    }
+
+
+
+    enum Cores
+    {
+        Branco = 1,
+        Vermelho,
+        Preto,
+        Cinza,
+        Prata,
+        Azul
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,18 @@
+﻿using Enum;
+
+Console.WriteLine("## Enum ##");
+
+Console.WriteLine($"{Cores.Branco} - {(int)Cores.Branco}");
+Console.WriteLine($"{Cores.Vermelho} - {(int)Cores.Vermelho}");
+Console.WriteLine($"{Cores.Preto} - {(int)Cores.Preto}");
+Console.WriteLine($"{Cores.Cinza} - {(int)Cores.Cinza}");
+Console.WriteLine($"{Cores.Prata} - {(int)Cores.Prata}");
+Console.WriteLine($"{Cores.Azul} - {(int)Cores.Azul}");
+
+Console.WriteLine("\n Selecione a cor do seu Carro");
+int cor = Convert.ToInt32(Console.ReadLine());
+
+Carro chevrolet = new("Sedan", "Chevrolet", "Onix", 2016, 110, cor);
+chevrolet.ExibirInfo();
+
+Console.ReadKey();
