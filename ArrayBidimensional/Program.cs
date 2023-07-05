182b0dea5dc3b094383b5d41d2f7e5be80631e01
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-07-05 11:35:25 -0300

    array bidmensional

---
+++ ArrayBidimensional/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("## Array Bidimensional ##");
+
+// Declaração e alocação de memória para o array
+string[,] alunos = new string[2,5]; // [i,j]
+
+for (int i = 0; i < alunos.GetLength(0); i++)
+{
+	for (int j = 0; j < alunos.GetLength(1); j++)
+	{
+        Console.Write($"Linha: {i} | Coluna: {j}\n");
+        alunos[i, j] = Console.ReadLine();
+
+    }
+}
+
+Console.WriteLine("Valores recebidos");
+
+for (int i = 0; i < alunos.GetLength(0); i++)
+{
+    for (int j = 0; j < alunos.GetLength(1); j++)
+    {
+        Console.Write($"[{i},{j}] = {alunos[i, j]}\t ");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
