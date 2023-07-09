822b97e9375db8441b2ffdf30e869b6c52b2a0dd
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-07-09 12:53:53 -0300

    listT e métodos

---
+++ ListT/MetodosFind.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListT
+{
+    internal class MetodosFind
+    {
+        public static void FindT()
+        {
+            // List<T> - Find(), FindLast(), FindIndex(), FindLastIndex(), FindAll()
+
+            // O método Find() retorna o primeiro elemento que corresponde ao predicado fornecido
+
+            // O método FindLast() retorna o último elemento que corresponde ao predicado fornecido.
+
+            // O método FindIndex() retorna o índice do primeiro elemento que corresponde ao predicado fornecido.
+            // Ele retorna -1 se não houver correspondência encontrada.
+
+            // O método FindLastIndex() retorna o índice do último elemento que corresponde ao predicado fornecido.
+            // Ele retorna -1 se não houve correspondência encontrada.
+
+            // O método FindAll() recupera todos os elementos que correspondem às condições definidas pelo predicado especificado.
+
+            // Um predicado é uma função/método de um argumento único que retorna um valor booleano
+
+            List<string> frutas = new() { "Uva", "Banana", "Pera", "Maçã", "Abacate", "Laranja", "Morango" };
+            var fruta1 = frutas.Find(Procurar);
+            Console.WriteLine(fruta1);
+
+            // usando o predicado como uma função
+            static bool Procurar(string item)
+            {
+                return item.Contains('n');
+            }
+
+            // Expresão lambda (função anônima)
+            var fruta2 = frutas.Find(f => f.StartsWith('L'));
+            Console.WriteLine(fruta2); // Laranja
+
+            var fruta3 = frutas.FindLast(f=> f.Contains('o'));
+            Console.WriteLine(fruta3); // Morango
+
+            var fruta4 = frutas.FindIndex(f => f == "Abacate");
+            Console.WriteLine(fruta4); // 4
+
+            var fruta5 = frutas.FindLastIndex(f=> f.Contains('n'));
+            Console.WriteLine(fruta5); // 6
+
+            var frutas6 = frutas.FindAll(f=> f.Contains('n'));
+            Console.WriteLine(String.Join(", ", frutas6)); // Banana, Laranja, Morango
+        }
+
+    }
+}
---
+++ ListT/MetodosList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListT
+{
+    class MetodosList
+    {
+        public static void ListT()
+        {
+            List<string> lista = new();
+            string[] array = { "Matheus", "Felipe" };
+            string[] array2 = { "Fernanda", "Camila" };
+            // List<T> - Adicionar elementos
+
+            // O método Add permite incluir um único elemento ao final de uma List<T>
+            // O método Insert(int index, Object value) permite incluir um elemento em uma posição qualquer usando um índicie específico
+            lista.Add("Carlos");
+            lista.Add("Paulo");
+            lista.Insert(0, "Márcia");
+
+            // AddRange(ICollection) - Adiciona a coleção de elementos ao final da lista
+            // InsertRange(int index, ICollection) - Adiciona a coleção de elementos na Lista na posição do índice especificado
+            lista.AddRange(array);
+            lista.InsertRange(0, array2);
+
+            // List<T> - Remover elementos
+
+            // Remove(Object value) - Remove a primeira ocorrência de um objeto específico
+            // RemoveAt(int index) - Remove o elemento no índice especificado
+            // RemoveRange(int index, int count) - Remove um número definido de elementos a partir de um índice especificado
+            lista.Remove("Felipe");
+
+            // List<T> - Acesso aos elementos
+
+            // Podemos acessar e atualizar os valores do elementos de uma List<T> usando o seu índice, da mesma forma que em um array.
+            // O índice começa de zero e aumenta em um para cada elemento subsequente.
+            Console.WriteLine(lista[0]);
+
+            // List<T> - Iteração usando o laço for
+
+            // Podemos iterar uma list<T> usando o laço for da mesma forma que nos arrays de uma dimensão. Usando a propriedade Count da List<T>
+            // para obter o número total de elementos.
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine($"i: {i} listaCount: {lista.Count}");
+                Console.WriteLine($"Count: {lista[i]}");
+            }
+
+            // List<T> - Iteração usando o laço foreach
+
+            // Podemos iterar uma List<T> usando o laço foreach da mesma forma que em um array (não precisamos usar índices)
+
+            foreach (var elemento in lista)
+            {
+                Console.WriteLine(elemento);
+            }
+
+            // List<T> - Verificar se um elemento está na coleção
+
+            // O método Contains() pode ser usado para determinar se um elemento especificado exista na List<T> ou não.
+            // O método retorna True se existir, caso contrário retorna False
+
+
+            lista.Contains("Matheus"); // True
+            lista.Contains("Ana"); // False
+
+            // List<T> - Verificar se um elemento está na coleção
+
+            // Para ordenar uma List<T> com elementos do mesmo tipo na ordem ascendente usamos o método Sort().
+            lista.Sort();
+            Console.WriteLine($"Lista ordenada: {String.Join(", ", lista)}");
+
+            // Para remover todos os elementos de uma List<T> sem alterar sua capacidade usamos o método Clear()
+            lista.Clear();
+            Console.WriteLine($"Quantidade de elementos: {lista.Count}");
+        }
+    }
+}
