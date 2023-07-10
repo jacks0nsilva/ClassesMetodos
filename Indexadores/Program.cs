4aae3ef8a85bc6967cdb544b54ff61000a05c579
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-07-10 10:32:58 -0300

    Indexadores

---
+++ Indexadores/Program.cs
@@ -0,0 +1,30 @@
+﻿using Indexadores;
+using System.Reflection.Metadata;
+
+Console.WriteLine("## Indexadores ##");
+
+Time time = new Time();
+// atribui valores = set
+time[1] = "Palmeiras";
+time[3] = "Santos";
+time[5] = "São Paulo";
+time[7] = "Vasco";
+time[-1] = "Flamengo";
+time[1000] = "Bahia";
+
+// obtendo os valores = get
+string valor1 = time[1];
+string valor2 = time[3];
+string valor3 = time[5];
+string valor4 = time[7];
+string valor5 = time[-1];
+string valor6 = time[1000];
+
+// exibir
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+
---
+++ Indexadores/Time.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexadores
+{
+    public class Time
+    {
+        // definir os limites do Array
+        string[] valor = new string[10];
+
+        public string this[int i] 
+        {
+            get 
+            { 
+                if(i>=0 && i < valor.Length)
+                {
+                    return valor[i];
+                }
+                return "Erro";
+            } 
+            
+            set 
+            { 
+                if(i>= 0 && i < valor.Length)
+                {
+                    valor[i] = value;
+                }
+            }
+        }
+
+    }
+}
