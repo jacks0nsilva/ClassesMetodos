9433fe002dda55b1e3fe0d0b652f20a05d785564
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-07-10 10:57:54 -0300

    classe random

---
+++ ClassRandom/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("## Classe Random ##");
+
+int[] numerosSorteados = new int[6];
+Random random = new Random();
+
+for (int i = 0; i < numerosSorteados.Length; i++)
+{
+	int numeroAleatorio;
+
+	do
+	{
+		numeroAleatorio = random.Next(1, 61); // Irá gerar um número de 1 a 60 e atribuir a variável numeroAleatorio
+	} while (numerosSorteados.Contains(numeroAleatorio)); // Verifica se há um número igual no array numerosSorteados, se houver, repete o loop do while
+
+	numerosSorteados[i] = numeroAleatorio;
+}
+
+Array.Sort(numerosSorteados); // Ordena o array de números sorteados
+
+Console.WriteLine("\nNúmeros sorteados\n");
+
+Console.Write(String.Join(" | ",numerosSorteados));
